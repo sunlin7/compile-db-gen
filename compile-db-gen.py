02b1253c3c1364bea3ea370e19f8078b5f2c11ab
Sunline <sunlin7@yahoo.com>
2018-12-01 06:18:47 +0800

    More code style change.

--- compile-db-gen.py
+++ compile-db-gen.py
@@ -3,16 +3,11 @@
 
 from __future__ import print_function
 import copy
-import itertools
 import json
 import os
 import re
-import shlex
 import subprocess
 import sys
-import tempfile
-import time
-import types
 
 compilers = [
     re.compile(r'^([^/]*/)*([^-]*-)*c(c|\+\+)$'),
@@ -24,13 +19,15 @@ accepted = {'.c', '.C', '.cc', '.CC', '.cxx', '.cp', '.cpp', '.c++',
             '.m', '.mm',
             '.i', '.ii', '.mii'}
 include_file = []
-include_dir  = []
+include_dir = []
 exclude_file = []
-exclude_dir  = []
+exclude_dir = []
+
 
 def compiler_call(executable):
     """ A predicate to decide the entry is a compiler call or not. """
     return any((pattern.match(executable) for pattern in compilers))
+
 
 def is_source_file(filename):
     """ A predicate to decide the filename is a source file or not. """
@@ -60,7 +57,10 @@ def join_command(args):
     """Join the command with escaped options."""
     return ' '.join([shell_escape(arg) for arg in args])
 
+
 g_sys_inc = {}
+
+
 def get_sys_inc(compiler):
     """return a list of compiler system include dir."""
     if compiler in g_sys_inc:
@@ -69,23 +69,31 @@ def get_sys_inc(compiler):
     lang = "c"
     if re.compile(r"\+\+|pp$").findall(compiler):
         lang = "c++"
-    p = subprocess.Popen([compiler, "-x", lang, "-E", "-v", "-"], stderr = subprocess.PIPE, stdin = subprocess.PIPE)
-    info = p.communicate(input='')[1]
-    raw_inc = re.compile(r"^.*starts here:((?:.|\n)*?)End of search list.", re.MULTILINE).findall(info.decode('utf-8'))
+
+    p = subprocess.Popen(
+        [compiler, "-x", lang, "-E", "-v", "-"],
+        stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    info = p.communicate(input='')[1].decode('utf-8')
+    raw_inc = re.compile(
+        r"^.*starts here:((?:.|\n)*?)End of search list.", re.MULTILINE).findall(info)
     if len(raw_inc) > 0:
         incs = re.compile("/.*$", re.MULTILINE).findall(raw_inc[0])
-        g_sys_inc[compiler] = [ "-I%s"%x for x in incs]
+        g_sys_inc[compiler] = ["-I%s" % x for x in incs]
     return g_sys_inc[compiler]
+
 
 class OType:
     CHILD = 1
     CHDIR = 2
     EXEC = 3
 
-chdir_re = re.compile (r"^(\d+) +chdir\((.*)\)\s+= 0")
-exec_re  = re.compile (r"^(\d+) +execve(\(.*\))\s+= 0")
-child_re = re.compile (r"^(\d+) .*SIGCHLD.*si_pid=(\d+).*")
+
+chdir_re = re.compile(r"^(\d+) +chdir\((.*)\)\s+= 0")
+exec_re = re.compile(r"^(\d+) +execve(\(.*\))\s+= 0")
+child_re = re.compile(r"^(\d+) .*SIGCHLD.*si_pid=(\d+).*")
 ccache_re = re.compile(r'^([^/]*/)*([^-]*-)*ccache(-\d+(\.\d+){0,2})?$')
+
+
 def genlineobjs(fname):
     """Parse the lines into objects."""
     objList = []
@@ -94,8 +102,8 @@ def genlineobjs(fname):
             m = chdir_re.match(line)
             if m is not None:   # chdir, record this
                 pid = m.group(1)
-                wd  = eval(m.group(2))
-                objList.append({'type':OType.CHDIR, 'pid':pid, 'wd':wd})
+                wd = eval(m.group(2))
+                objList.append({'type': OType.CHDIR, 'pid': pid, 'wd': wd})
                 # print (pid + " chdir:" + proc_run[pid]["cwd"])
                 continue
 
@@ -103,21 +111,26 @@ def genlineobjs(fname):
             if m is not None:   # the child process end, move it to it's parent
                 pid = m.group(1)
                 cid = m.group(2)
-                objList.append({'type':OType.CHILD, 'pid':pid, 'cid':cid})
+                objList.append({'type': OType.CHILD, 'pid': pid, 'cid': cid})
                 continue
 
             m = exec_re.match(line)
             if m is not None:   # execve, get the compiler
                 pid = m.group(1)
-                # for strace <=4.11, format: 012 execve("PATH", ["E", "..."], [/* N vars */]) = 0
-                # for strace 2018, format:   012 execve("PATH", ["E", "..."], 0xM /* N vars */) = 0
+                # for strace <=4.11, format:
+                #  012 execve("PATH", ["E", "..."], [/* N vars */]) = 0
+                # for strace 2018, format:
+                #  012 execve("PATH", ["E", "..."], 0xM /* N vars */) = 0
                 # remove the tail of execve()
-                line = re.sub(", \[/\* [^*]+ \*/\]", "", m.group(2))
-                line = re.sub(', 0x[^\)]+', '', line)
+                line = re.sub(r", \[/\* [^*]+ \*/\]", "", m.group(2))
+                line = re.sub(r', 0x[^\)]+', '', line)
                 (programName, command) = eval(line)
                 if ccache_re.match(programName) is not None \
                    or compiler_call(programName):
-                   objList.append({'type':OType.EXEC, 'pid':pid, 'progName':programName, 'command': command}) 
+                    objList.append({'type': OType.EXEC,
+                                    'pid': pid,
+                                    'progName': programName,
+                                    'command': command})
 
     return objList
 
@@ -131,7 +144,7 @@ def getParentPid(itrPidObj, pid):
     return None
 
 
-def parse_exec_trace(proc_run, fname, auto_sys_inc = False):
+def parse_exec_trace(proc_run, fname, auto_sys_inc=False):
     """Construct the compile tree, and the key is pid, the node contain
 proc_run[pid] = {
 'cwd':'',   # the last chdir, the child process depend on this value
@@ -148,53 +161,69 @@ proc_run[pid] = {
             break
 
         pid = item['pid']
-        if pid not in proc_run:           # first ocurr in the lines, new child process, get the dir
-            ppid = getParentPid(copy.copy(itr), pid)  # try to find the child end log to get its parent
+        if pid not in proc_run:
+            # first ocurr in the lines, it's new child process, get the dir
+            # try to find the child end log to get its parent
+            ppid = getParentPid(copy.copy(itr), pid)
             cwd = proc_run[ppid]['cwd'] if ppid in proc_run else ""
-            proc_run[pid] = {"cwd":cwd, "child":[], "cmds":[]}
+            proc_run[pid] = {"cwd": cwd, "child": [], "cmds": []}
 
         if item['type'] == OType.CHDIR:   # chdir, record this
-            proc_run[pid]["cwd"] = os.path.join(proc_run[pid]["cwd"], item['wd'])
-            # print(pid + " chdir:" + proc_run[pid]["cwd"]) 
+            proc_run[pid]["cwd"] = os.path.join(proc_run[pid]["cwd"],
+                                                item['wd'])
+            # print(pid + " chdir:" + proc_run[pid]["cwd"])
             continue
 
-        if item['type'] == OType.CHILD:   # the child process end, move it to it's parent
+        if item['type'] == OType.CHILD:
+            # the child process end, move it to it's parent
             pid = item['pid']
             cid = item['cid']
             if cid in proc_run:
                 item = proc_run[cid]
-                del proc_run[cid] # remove from 'running' process list
+                del proc_run[cid]  # remove from 'running' process list
 
-                if not pid in proc_run: # this process end, append it to it's parent
-                    proc_run[pid] = {"cwd":"", "child":[], "cmds": []}
+                if pid not in proc_run:
+                    # this process end, append it to it's parent
+                    proc_run[pid] = {"cwd": "", "child": [], "cmds": []}
                 # print(pid + " child_end:" + item["cwd"])
-                proc_run[pid]["child"].append({cid:item})
+                proc_run[pid]["child"].append({cid: item})
             continue
 
         if item['type'] == OType.EXEC:   # execve, get the compiler
             pid = item['pid']
             programName, command = item['progName'], item['command']
             if ccache_re.match(programName) is not None:
-                programName = command[1] # for "ccache", drop first slot (which is "ccache")
+                # for "ccache", drop first slot (which is "ccache")
+                programName = command[1]
                 del command[0]
- 
+
             if compiler_call(programName):
-                if len(command) >= 2 and command[1] == "-cc1": # ignore the "clang -cc1 ..." call
+                if len(command) >= 2 and command[1] == "-cc1":
+                    # ignore the "clang -cc1 ..." call
                     continue
 
                 sys_inc = []
                 if auto_sys_inc:
                     sys_inc = get_sys_inc(command[0])
 
-                for f in command: # make item for each
+                for f in command:  # make item for each
                     if is_source_file(f):
-                        if not pid in proc_run:
-                            proc_run[pid] = {"cwd":"", "child":[], "cmds": []}
+                        if pid not in proc_run:
+                            proc_run[pid] = {
+                                "cwd": "",
+                                "child": [],
+                                "cmds": []
+                            }
 
                         # print pid + " execv:" + proc_run[pid]["cwd"]
-                        proc_run[pid]["cmds"].append({"directory":proc_run[pid]["cwd"],
-                                                     "command": join_command(command  + sys_inc),
-                                                     "file": f})
+                        cmds = join_command(command + sys_inc)
+                        jstr = shell_quote(cmds)
+                        proc_run[pid]["cmds"].append({
+                            "directory": proc_run[pid]["cwd"],
+                            "command": jstr,
+                            "file": f
+                        })
+
 
 def print_exec_trace(proc_run, cwd, proc_res):
     """Print the execute trace in compile data json format."""
@@ -205,51 +234,57 @@ def print_exec_trace(proc_run, cwd, proc_res):
         for child in proc["child"]:
             print_exec_trace(child, wd, proc_res)
         for cmd in proc_run[pid]["cmds"]:
-            d = wd; #os.path.join(wd, cmd["directory"])
+            d = wd              # os.path.join(wd, cmd["directory"])
             cmd["directory"] = d
             f = cmd["file"]
-            if len(include_file) > 0 and not(any((r.search(f) for r in include_file))):
+            if len(include_file) > 0 \
+               and not (any((r.search(f) for r in include_file))):
                 continue
-            if len(exclude_file) > 0 and any((r.search(f) for r in exclude_file)):
+            if len(exclude_file) > 0 \
+               and any((r.search(f) for r in exclude_file)):
                 continue
-            if len(include_dir) > 0 and not(any((r.search(d) for r in include_dir))):
+            if len(include_dir) > 0 \
+               and not(any((r.search(d) for r in include_dir))):
                 continue
-            if len(exclude_dir) > 0 and any((r.search(d) for r in exclude_dir)):
+            if len(exclude_dir) > 0 \
+               and any((r.search(d) for r in exclude_dir)):
                 continue
             proc_res.append(cmd)
+
 
 def trace(args):
     """Trace the compile command and get the raw compile log."""
     # request strace-4.8 or higher
-    p = subprocess.Popen(["strace", "-V"], stdout = subprocess.PIPE)
-    p.wait();
+    p = subprocess.Popen(["strace", "-V"], stdout=subprocess.PIPE)
+    p.wait()
     sVer = p.stdout.read().decode('utf-8')
     # for Ubuntu 18.04, the ver string is "version UNKNOWN"
-    mVer = re.match("strace -- version (\d+)\.(\d+)", sVer)
+    mVer = re.match(r'strace -- version (\d+)\.(\d+)', sVer)
     if mVer:
         major = int(mVer.group(1))
-        if major < 4 or (major == 4 and int(mVer.group(2))  < 8):
+        if major < 4 or (major == 4 and int(mVer.group(2)) < 8):
             print("strace version should high than 4.8")
             print("Current:" + sVer)
             sys.exit(1)
 
-    p = subprocess.Popen(["getconf", "ARG_MAX"], stdout = subprocess.PIPE)
+    p = subprocess.Popen(["getconf", "ARG_MAX"], stdout=subprocess.PIPE)
     p.wait()
-    arg_max = str(int(p.stdout.readline ()))
-    command  = ["strace", "-f", "-s" + arg_max, "-etrace=execve,chdir", "-o", args.output]
+    arg_max = str(int(p.stdout.readline()))
+    command = ["strace", "-f", "-s" + arg_max,
+               "-etrace=execve,chdir", "-o", args.output]
     command += args.command
-    #TBD: the output of stdin/stderr maybe very large, hangup happend when try
+    # TBD: the output of stdin/stderr maybe very large, hangup happend when try
     # to grabe them, refer the manual of .wait() for detail.
-    p = subprocess.Popen(command);
-    p.wait();
+    p = subprocess.Popen(command)
+    p.wait()
 
     return p.returncode
 
+
 def parse(args):
     """Parse the output from trace and generate the compile_commands.json."""
-    proc_end = {}
     proc_run = {}
-    fname= args.raw_database
+    fname = args.raw_database
     if args.startup_dir is not None:
         cwd = os.path.abspath(args.startup_dir)
     else:
@@ -271,115 +306,121 @@ def parse(args):
     print_exec_trace(proc_run, cwd, proc_res)
     json.dump(proc_res, fs, indent=4)
 
+
 def run(args):
     """run the build command and generate the compilation database."""
     raw_database = "./compile_commands.raw"
     output = args.output
     args.output = raw_database
-    if trace (args) == 0:
+    if trace(args) == 0:
         args.output = output    # restore the value
         args.raw_database = raw_database
-        parse (args)
+        parse(args)
+
 
 def add_common_opts_parse(s):
     """add the opts for subcommand "parse" """
-    s.add_argument (
+    s.add_argument(
         "--startup-dir", "-s",
-        default = None,
-        help = "the startup directory")
-    s.add_argument (
+        default=None,
+        help="the startup directory")
+    s.add_argument(
         "--auto-sys-inc", "-a",
-        default = True,
-        action = "store_true",
-        help = "auto detect the system include path")
-    s.add_argument (
+        default=True,
+        action="store_true",
+        help="auto detect the system include path")
+    s.add_argument(
         "--include", "-i",
-        metavar = "REGEX",
-        default = [],
-        action = "append",
-        help = "include the file parten")
+        metavar="REGEX",
+        default=[],
+        action="append",
+        help="include the file parten")
     s.add_argument (
         "--exclude", "-e",
-        metavar = "REGEX",
-        default = [],
-        action = "append",
-        help = "exclude the file patten")
-    s.add_argument (
+        metavar="REGEX",
+        default=[],
+        action="append",
+        help="exclude the file patten")
+    s.add_argument(
         "--include-dir", "-I",
-        metavar = "REGEX",
-        default = [],
-        action = "append",
-        help = "include the dir parten")
-    s.add_argument (
+        metavar="REGEX",
+        default=[],
+        action="append",
+        help="include the dir parten")
+    s.add_argument(
         "--exclude-dir", "-E",
-        metavar = "REGEX",
-        default = [],
-        action = "append",
-        help = "exclude the dir patten")
+        metavar="REGEX",
+        default=[],
+        action="append",
+        help="exclude the dir patten")
+
 
 def add_common_opts_trace(s):
     """add the opts for subcommand "trace" """
-    s.add_argument (
+    s.add_argument(
         "command",
-        metavar = "COMMAND",
-        nargs = argparse.REMAINDER,
-        help = "build command line")
+        metavar="COMMAND",
+        nargs=argparse.REMAINDER,
+        help="build command line")
+
 
 def main():
     "The main function"
 
-    parser = argparse.ArgumentParser(description = "Generate the compile database from build")
-    subparsers = parser.add_subparsers(metavar = "SUBCOMMAND")
+    parser = argparse.ArgumentParser(
+        description="Generate the compile database from build")
+    subparsers = parser.add_subparsers(metavar="SUBCOMMAND")
 
     # run the compile command and generate the JSON compilation database
-    s = subparsers.add_parser (
+    s = subparsers.add_parser(
         "run",
-        help = "(Default) trace build command, and parse result ",
-        description = "Create a compilation database by tracing a build command.")
-    add_common_opts_parse (s)
-    add_common_opts_trace (s)
-    s.add_argument (
+        help="(Default) trace build command, and parse result ",
+        description="Create a compilation database by tracing build command.")
+    add_common_opts_parse(s)
+    add_common_opts_trace(s)
+    s.add_argument(
         "--output", "-o",
-        default = "./compile_commands.json",
-        help = "the strace output file")
-    s.set_defaults (sourceType = "run")
-    s.set_defaults (fun = run)
+        default="./compile_commands.json",
+        help="the strace output file")
+    s.set_defaults(sourceType="run")
+    s.set_defaults(fun=run)
 
     # trace
-    s = subparsers.add_parser (
+    s = subparsers.add_parser(
         "trace",
-        help = "trace build command",
-        description = "Create a compilation database by tracing a build command.")
-    s.add_argument (
+        help="trace build command",
+        description="Create a compilation database by tracing build command.")
+    s.add_argument(
         "--output", "-o",
-        default = "./compile_commands.raw",
-        help = "the strace output file")
-    add_common_opts_trace (s)
-    s.set_defaults (sourceType = "trace")
-    s.set_defaults (fun = trace)
+        default="./compile_commands.raw",
+        help="the strace output file")
+    add_common_opts_trace(s)
+    s.set_defaults(sourceType="trace")
+    s.set_defaults(fun=trace)
 
     # parse
-    s = subparsers.add_parser (
+    s = subparsers.add_parser(
         "parse",
-        help = "parse the strace file",
-        description = "Create a compilation database from the result by tracing a build command.")
-    add_common_opts_parse (s)
-    s.add_argument (
+        help="parse the strace file",
+        description="Create compilation database from the tracking log.")
+    add_common_opts_parse(s)
+    s.add_argument(
         "raw_database",
-        default = "./compile_commands.raw",
-        nargs = '?',
-        help = "the raw database from strace")
-    s.add_argument (
+        default="./compile_commands.raw",
+        nargs='?',
+        help="the raw database from strace")
+    s.add_argument(
         "output",
-        default = "./compile_commands.json",
-        nargs = '?',
-        help = "the output compilor database")
-    s.set_defaults (sourceType = "parse")
-    s.set_defaults (fun = parse)
+        default="./compile_commands.json",
+        nargs='?',
+        help="the output compilor database")
+    s.set_defaults(sourceType="parse")
+    s.set_defaults(fun=parse)
 
-    parser.set_default_subparser(len(os.sys.argv) <= 1 and "-h" or "run") # set default subcommand after all subcommand ready
-    args = parser.parse_args ()
-    return args.fun (args)
+    # set default subcommand after all subcommand ready
+    parser.set_default_subparser(len(os.sys.argv) <= 1 and "-h" or "run")
+    args = parser.parse_args()
+    return args.fun(args)
 
 # from http://stackoverflow.com/questions/6365601/default-sub-command-or-handling-no-sub-command-with-argparse
 def set_default_subparser(self, name, args=None):
@@ -408,6 +449,7 @@ def set_default_subparser(self, name, args=None):
                 sys.argv.insert(1, name)
             else:
                 args.insert(0, name)
+
 
 if __name__ == "__main__":
     try:
